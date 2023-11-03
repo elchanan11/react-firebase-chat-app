7c9487016ecdeab0c3efea4cd53e9536313039da
elchanan11 <elchanan.elsner@gmail.com>
2023-11-03 18:24:51 +0200

    Add user data to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,20 @@
 import profileImg from "../img/profile-pic.png"
 import {signOut} from "firebase/auth"
 import {auth} from "../firebase";
-
+import {useContext} from "react";
+import {AuthContext} from "../context/AuthContext";
+import {Person} from "@mui/icons-material";
 
 function NavBar() {
+    const {currentUser} = useContext(AuthContext)
+    console.log(currentUser)
+
     return (
         <div className={"navBar"}>
             <span className={"logoNav"}>EL Chat</span>
             <div className={"user"}>
-                <img className={"imgNav"} src={profileImg} alt={""}/>
-                <span className={"navName"}>Jhon</span>
+                <img className={"imgNav"} src={currentUser.photoURL ? currentUser.photoURL : <Person />} alt={""}/>
+                <span className={"navName"}>{currentUser.displayName}</span>
                 <button className={"buttonNav"} onClick={() => signOut(auth)}>logout</button>
             </div>
         </div>
