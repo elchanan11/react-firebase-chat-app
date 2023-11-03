c83add78135c06a7a1c6e39a0083c8c0cd5d9111
elchanan11 <elchanan.elsner@gmail.com>
2023-11-03 13:07:57 +0200

    Add basic app design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import './App.css';
-import Reagister from "./pages/Reagister";
 import "./style.css"
-import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 function App() {
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,16 @@
+import profileImg from "../img/profile-pic.png";
+
 function Message() {
     return (
-        <div className={"message"}>
-            sjsjx
+        <div className={"message owner"}>
+            <div className={"messageInfo"}>
+                <img className={"imageMessageInfo"} src={profileImg} alt={""}/>
+                <span className={"messageInfoTime"}>Just Now</span>
+            </div>
+            <div className={"messageContent"}>
+                <p className={"messageContentText"}>Hello</p>
+                <img className={"imageMessageContent"} src={profileImg} alt={""}/>
+            </div>
         </div>
     )
 }
